e60a92f48c0af305d50d58c46575212e50a8555e
abailie3 <albgk93@gmail.com>
2018-01-27 14:36:38 -0600

    More codecov integration. Moved test_all to main directory for ease of use.

--- test_all.py
+++ test_all.py
@@ -4,13 +4,13 @@ Copyright (c) 2018 Austin Bailie, All rights reserved.
 This is the main testing class. All tests should be added to the suite method below.
 See other included test modules for notes on how to configure sub-tests.
 """
-import unittest
-
 from tests import test_bruteforcemethod
 from tests import test_distance
 from tests import test_minterface
 from tests import test_tsproblem
 from tests import test_tsvisualizer
+import unittest
+
 
 def suite():
     """
@@ -25,6 +25,7 @@ def suite():
     suite.addTest(test_distance.suite())
     suite.addTest(test_tsvisualizer.suite())
     return suite
+
 
 if __name__ == "__main__":
     unittest.main(defaultTest='suite')
--- tests/context.py
+++ tests/context.py
@@ -4,6 +4,6 @@ Copyright (c) 2018 Austin Bailie, All rights reserved.
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-import travelingintelligence
-from travelingintelligence import machinelibrary
+#
+# import travelingintelligence
+# from travelingintelligence import machinelibrary
